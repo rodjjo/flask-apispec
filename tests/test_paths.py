b2f8c5cb56370d5bcd226f39da18e95a93f4870a
Rodrigo Antônio de Araújo <rodjjo@gmail.com>
2017-01-26 00:58:54 -0200

    Update test_paths.py

--- tests/test_paths.py
+++ tests/test_paths.py
@@ -59,8 +59,8 @@ class TestPathParams:
  
     def test_params_override_header(self, app):
         rule = make_rule(app, '/some_path/')
-        overrides = {'Authorization': {'description': 'The authorization token', "in": "header"}}
+        overrides = {'Authorization': {'description': 'The authorization token', 'in': 'header', 'required': True}}
         params = rule_to_params(rule, overrides=overrides)
-        expected = make_param(type='string', name='Authorization', in_location="header", description='The authorization token',
+        expected = make_param(type='string', name='Authorization', in_location='header', description='The authorization token',
                               required=True)
         assert params[0] == expected
